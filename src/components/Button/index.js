36ccf670aedbbc38c7b584bba2982cd68b5719f5
lenh0205 <lenhdeptrai98@gmail.com>
2022-06-14 22:26:52 +0700

    111. tìm hiểu thư viện proptypes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
+import PropTypes from 'prop-types'
 
 const cx = classNames.bind(styles);
 
@@ -65,6 +66,10 @@ function Button({
             {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
         </Comp>
     );
+}
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired
 }
 
 export default Button;
